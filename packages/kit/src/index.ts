a2ac074dcb856c7c221b0a92a963158e2ea2da8e
CSenshi <saba.pochkhua@gmail.com>
2025-08-10 22:26:03 +0400

    feat(kit): initialize package

---
+++ packages/kit/src/index.ts
@@ -0,0 +1,4 @@
+export * from '@nestjs-redis/client';
+export { RedisHealthIndicator } from '@nestjs-redis/health-indicator';
+export * from '@nestjs-redis/throttler-storage';
+export * from '@nestjs-redis/lock';
