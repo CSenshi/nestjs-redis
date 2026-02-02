a952365368069aa4508463b8aa6fc163af188e00
CSenshi <saba.pochkhua@gmail.com>
2026-02-02 17:48:28 +0400

    refactor(kit): remove deprecated package and configuration files

--- packages/kit/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from '@nestjs-redis/client';
-export { RedisHealthIndicator } from '@nestjs-redis/health-indicator';
-export * from '@nestjs-redis/throttler-storage';
-export * from '@nestjs-redis/lock';
-export * from '@nestjs-redis/socket.io-adapter';
