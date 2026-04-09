788acf12bd6969e50f494301d34e6bb0c2069b45
CSenshi <saba.pochkhua@gmail.com>
2026-04-09 20:53:32 +0400

    feat(scheduler): add schedule constants and enums

---
+++ packages/scheduler/src/lib/enums/index.ts
@@ -0,0 +1,2 @@
+export { CronExpression } from './cron-expression.enum.js';
+export { SchedulerType } from './scheduler-type.enum.js';
---
+++ packages/scheduler/src/lib/enums/scheduler-type.enum.ts
@@ -0,0 +1,5 @@
+export enum SchedulerType {
+  CRON = 'CRON',
+  INTERVAL = 'INTERVAL',
+  TIMEOUT = 'TIMEOUT',
+}
