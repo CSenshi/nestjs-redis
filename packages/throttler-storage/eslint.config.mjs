7baf7277e2a6dfdebf9a296bf64bb4c3c51d0b8c
CSenshi <saba.pochkhua@gmail.com>
2026-07-22 17:25:59 -0700

    chore: replace Nx ESLint and format with standard tooling

--- packages/throttler-storage/eslint.config.mjs
+++ packages/throttler-storage/eslint.config.mjs
@@ -1,23 +1,3 @@
 import baseConfig from '../../eslint.config.mjs';
 
-export default [
-  ...baseConfig,
-  {
-    files: ['**/*.json'],
-    rules: {
-      '@nx/dependency-checks': [
-        'error',
-        {
-          ignoredFiles: [
-            '{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}',
-            '{projectRoot}/**/*.spec.ts',
-          ],
-          ignoredDependencies: ['redis', '@nestjs/throttler'],
-        },
-      ],
-    },
-    languageOptions: {
-      parser: await import('jsonc-eslint-parser'),
-    },
-  },
-];
+export default [...baseConfig];
