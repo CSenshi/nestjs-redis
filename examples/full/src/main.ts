c57d8ffec3769ebfa4b16bc2e1546e7baae7438f
CSenshi <saba.pochkhua@gmail.com>
2026-08-30 23:25:46 +0400

    fix(example): add missing socket io

--- examples/full/src/main.ts
+++ examples/full/src/main.ts
@@ -4,6 +4,7 @@
  */
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { setupRedisAdapter } from '@nestjs-redis/socket.io-adapter';
 import { AppModule } from './app/app.module.js';
 
 async function bootstrap() {
@@ -11,6 +12,7 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+  await setupRedisAdapter(app);
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
